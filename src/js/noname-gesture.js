54532f205b40427461a982c219a421aac54ad863
18223781723 <1461301522@qq.com>
2021-06-20 18:19:57 +0800

    创建文件

---
+++ src/js/noname-gesture.js
@@ -0,0 +1,21 @@
+
+function NonameGesture(element, options) {
+	this.x = this.y = 0;
+	this.x2 = this.y2 = 0;
+	this.longTapTimer = null;
+}
+NonameGesture.prototype.init = function () {
+
+}
+NonameGesture.prototype.handleTouchStart = function () {
+
+}
+NonameGesture.prototype.handleTouchMove = function () {
+
+}
+NonameGesture.prototype.handleTouchEnd = function () {
+
+}
+NonameGesture.prototype.handleTouchCancel = function () {
+
+}
