1c4f3be39d13f9f2b9b0cde6cb318eec47fb7674
18223781723 <1461301522@qq.com>
2021-06-25 19:32:56 +0800

    增加滑动事件逻辑

--- src/js/noname-gesture.js
+++ src/js/noname-gesture.js
@@ -1,32 +1,25 @@
 const nonameGesture = {
 	element: null,
 	start: { x: 0, y: 0 },
-	start2: {
-		x: 0, y: 0
-	},
-	step: {
-		x: 0, y: 0
-	},
-	distance: {
-		x: 0, y: 0
-	},
-	lastDistance: {
-		x: 0, y: 0
-	},
+	start2: { x: 0, y: 0 },
+	step: { x: 0, y: 0 },
+	distance: { x: 0, y: 0 },
+	lastDistance: { x: 0, y: 0 },
 	lastMove: {},
 	lastCenter: {},
 	direction: '',
 	tapCount: 0,
-	tapTimer: null,
-	longTapTimer: null,
+	points: [],
+	tapTimeout: null,
+	longTapTimeout: null,
 	options: null,
 	/**
 	 * 初始化执行
-	 * @param {HTMLElement} el 元素
+	 * @param {HTMLElement} element 元素
 	 * @param {object} options 配置项
 	 */
-	init: function (el, options) {
-		this.element = el;
+	init: function (element, options) {
+		this.element = element;
 		this.options = options;
 		this.bindEvent();
 	},
@@ -38,18 +31,18 @@ const nonameGesture = {
 		this.start = { x: e.touches[0].clientX, y: e.touches[0].clientY };
 		this.lastMove = { x: e.touches[0].clientX, y: e.touches[0].clientY };
 		if (e.touches.length === 1) {
+			this.points = [];
 			this.tapCount++;
-			clearTimeout(this.tapTimer);
+			clearTimeout(this.tapTimeout);
 			if (this.tapCount === 1) {
-				this.longTapTimer = setTimeout(() => {
+				this.longTapTimeout = setTimeout(() => {
 					this.tapCount = 0;
 					if (this.options.longTap) this.options.longTap();
-				}, 600);
+				}, 750);
 			}
-
 		} else if (e.touches.length === 2) {
 			this.tapCount = 0;
-			clearTimeout(this.longTapTimer);
+			clearTimeout(this.longTapTimeout);
 		}
 	},
 	/**
@@ -57,6 +50,7 @@ const nonameGesture = {
 	 * @param {TouchEvent} e
 	 */
 	handleTouchMove: function (e) {
+		// console.log(e);
 		const current = { x: e.touches[0].clientX, y: e.touches[0].clientY };
 		if (e.touches.length === 1) {
 			this.step = { x: current.x - this.lastMove.x, y: current.y - this.lastMove.y };
@@ -65,25 +59,50 @@ const nonameGesture = {
 			// 由于手指目标相对较大，偏移量<10认定为没有移动
 			if (Math.abs(this.distance.x) > 10 || Math.abs(this.distance.y) > 10) {
 				this.tapCount = 0;
-				clearTimeout(this.longTapTimer);
+				clearTimeout(this.longTapTimeout);
+				if (this.points.length === 20) this.points.pop();
+				this.points.unshift({ x: current.x, y: current.y, timeStamp: e.timeStamp });
 				if (this.options.move) {
 					this.options.move({ distance: { x: this.distance.x, y: this.distance.y }, step: { x: this.step.x, y: this.step.y } });
 				}
 			}
 		} else {
-			e.preventDefault();
+
 		}
+		e.preventDefault();
 	},
 	/**
 	 * 处理touchend
 	 * @param {TouchEvent} e
 	 */
 	handleTouchEnd: function (e) {
-		e.preventDefault();
 		if (e.touches.length === 0) {
-			clearTimeout(this.longTapTimer);
-			if (this.tapCount === 1) {
-				this.tapTimer = setTimeout(() => {
+			clearTimeout(this.longTapTimeout);
+			if (this.tapCount === 0) {
+				const swipeDistance = { x: 0, y: 0 };
+				let direction = '';
+				for (const item of this.points) {
+					if (e.timeStamp - item.timeStamp > 100) break;
+					swipeDistance.x = e.changedTouches[0].clientX - item.x;
+					swipeDistance.y = e.changedTouches[0].clientY - item.y;
+				}
+				if (Math.abs(swipeDistance.x) > 30) {
+					if (swipeDistance.x > 0) {
+						direction = 'right';
+					} else {
+						direction = 'left'
+					}
+					if (this.options.swipe) this.options.swipe(direction);
+				} else if (Math.abs(swipeDistance.y) > 30) {
+					if (swipeDistance.y > 0) {
+						direction = 'down';
+					} else {
+						direction = 'up'
+					}
+					if (this.options.swipe) this.options.swipe(direction);
+				}
+			} else if (this.tapCount === 1) {
+				this.tapTimeout = setTimeout(() => {
 					this.tapCount = 0;
 					if (this.options.tap) this.options.tap();
 				}, 300);
@@ -96,6 +115,7 @@ const nonameGesture = {
 			this.lastMove = { x: e.touches[0].clientX, y: e.touches[0].clientY };
 			this.lastDistance = { x: this.distance.x, y: this.distance.y };
 		}
+		// e.preventDefault();
 	},
 	/**
 	 * 处理touchcancel
